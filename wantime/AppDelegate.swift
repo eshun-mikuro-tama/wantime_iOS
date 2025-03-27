9d443c66c167b3cf9e1d5a8363f3af655fc6c850
shunsuke.iida@gmail.com <167662673+eshun-mikuro-tama@users.noreply.github.com>
2025-03-28 02:55:07 +0900

    IOSアップデート google広告設定

---
+++ wantime/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  wantime
+//
+//  Created by 飯田俊輔 on 2025/03/22.
+//
+
+import Foundation
---
+++ wantime/InterstitialAdManager.swift
@@ -0,0 +1,8 @@
+//
+//  InterstitialAdManager.swift
+//  wantime
+//
+//  Created by 飯田俊輔 on 2025/03/22.
+//
+
+import Foundation
